36944ca2ec7cc8a2a25aca39bfdf8b70d01aa961
Bearei <bear_ei@outlook.com>
2022-12-02 14:09:02 +0800

    fix: fix interface error (#27)
    * chore: upgrade dependency
    
    * feat: add custom button ref
    
    * feat: version  0.0.11
    
    * feat: add multiple buttons for props
    
    * feat: version 0.0.13
    
    * feat: version 0.0.14
    
    * docs: Update the document
    
    * style: Format the import
    
    * feat: version 0.0.15
    
    * fix: Fix the type error
    
    * feat: version 0.0.17
    
    * feat: version 0.0.18
    
    * feat: version 0.0.19
    
    * feat: Update the interface
    
    * feat: fix interface error
    
    * fix: fix ref type binding error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import {bindEvents, handleDefaultEvent} from '@bearei/react-util/lib/event';
-import {useId, ButtonHTMLAttributes, DetailedHTMLProps, ReactNode, Ref, TouchEvent} from 'react';
+import {ButtonHTMLAttributes, DetailedHTMLProps, ReactNode, Ref, TouchEvent, useId} from 'react';
 import type {GestureResponderEvent, TouchableHighlightProps} from 'react-native';
 
 /**
@@ -93,7 +93,7 @@ export interface ButtonProps<T> extends BaseButtonProps<T> {
   /**
    * Render the button main
    */
-  renderMain?: (props: ButtonMainProps) => ReactNode;
+  renderMain?: (props: ButtonMainProps<T>) => ReactNode;
 
   /**
    * Render the button container
@@ -113,7 +113,7 @@ export interface ButtonChildrenProps extends Omit<BaseButtonProps, 'ref'> {
 }
 
 export type ButtonIconProps = ButtonChildrenProps;
-export type ButtonMainProps = ButtonChildrenProps & Pick<BaseButtonProps, 'ref'>;
+export type ButtonMainProps<T> = ButtonChildrenProps & Pick<BaseButtonProps<T>, 'ref'>;
 export type ButtonContainerProps = ButtonChildrenProps;
 
 const Button = <T extends HTMLElement>(props: ButtonProps<T>) => {
